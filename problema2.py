af73937caa58c02c7ed597125f03160c07824146
Edith <edith65elisa@gmail.com>
2024-07-13 14:46:47 -0300

    puse una carita feliz

--- problema2.py
+++ problema2.py
@@ -2,7 +2,7 @@
 hermano1=16
 hermano2=16
 if hermano1 > hermano2:   
-  print("hermano1 es mayor que hermano2")
+  print("hermano1 es mayor que hermano2 :D")
 elif hermano2 > hermano1:
   print("hermano2 es mayor que hermano1")
 else:
